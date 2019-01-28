03f3020faf5dcca4478a26630e5d324689baf76a
tamami <tamami.oka@gmail.com>
2019-01-28 14:32:50 +0700

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp()
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> with SingleTickerProviderStateMixin {
+
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = new AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 5000
+      )
+    );
+
+    animation = new Tween(
+      begin: 0.0,
+      end: 400.0
+    ).animate(controller);
+    animation.addListener(() {
+      setState(() {
+        // the state of the animation has changed
+
+      });
+    });
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        height: animation.value,
+        width: animation.value,
+        child: new Center(
+          child: new FlutterLogo(size: 300.0)
+        ),
+      ),
+    );
+  }
+}
